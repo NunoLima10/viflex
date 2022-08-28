c0c5a21c6f2ee7e5ddff2881ca1db84ef341e84c
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-08-28 13:18:25 -0100

    feat: add color printer

---
+++ color_printer.py
@@ -0,0 +1,20 @@
+
+import colorama
+
+class ColorPrinter:
+    def __init__(self) -> None:
+        colorama.init(autoreset=True)
+
+        self.styles = {
+            "success":colorama.Fore.GREEN + colorama.Style.BRIGHT,
+            "warning":colorama.Fore.YELLOW + colorama.Style.BRIGHT,
+            "error":colorama.Fore.RED + colorama.Style.BRIGHT
+        }
+
+    def show(self, text: str, type: str = "success") -> None:
+        message = text
+        if type in self.styles:
+            message = f'{self.styles[type]}{text}'
+        print(message)
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
-def main() -> None:
-    print("hello vidloader")
+from color_printer import ColorPrinter
 
+def main() -> None:
+    printer =  ColorPrinter()
+
+    printer.show("text")
+    printer.show("success","success")
+    printer.show("warning","warning")
+    printer.show("error","error")
 
 if __name__=="__main__":
     main()
