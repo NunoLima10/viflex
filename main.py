1852c5a3c3c2f61589c986dd16186c5812463ae9
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-08-28 14:44:30 -0100

    feat: add arg parser

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 
 from color_printer import ColorPrinter
 
+import argparse
+
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v","-video",help="Download format",action="store_true")
+    parser.add_argument("-m","-music",help="Download format",action="store_true")
+    parser.add_argument("link",help="Video link",type=str)
+    
+    return parser.parse_args()
+
+
 def main() -> None:
-    printer =  ColorPrinter()
-
-    printer.show("text")
-    printer.show("success","success")
-    printer.show("warning","warning")
-    printer.show("error","error")
-
+   args =  arg_parser()
+  
 if __name__=="__main__":
     main()
